46450ffd6dcac7a0960c0585518e2dbf2462e440
DH6688 <90415131+DH6688@users.noreply.github.com>
2023-08-16 15:11:56 -0700

    Create descriptions.py

---
+++ descriptions.py
@@ -0,0 +1,91 @@
+import streamlit as st
+
+st.title("12 Angry LLMs Role Description")
+
+st.subheader("Juror No. 1: Davis")
+st.image(image="https://raw.githubusercontent.com/DH6688/avatar_images/main/Davis.png", width=175)
+st.write("Davis is a quiet, thoughtful, gentle man. A man who sees all sides of every question and constantly seeks the truth. A man of strength tempered with compassion. \
+Above all, he is a man who wants justice to be done and will fight to see that it is. An architect, father, and the single initial “not guilty” \
+vote. Davis argues that the men should take their time in coming to a verdict. He slowly and painstakingly examines the evidence and \
+testimony to decide if there are grounds for a conviction. Davis is patient and compassionate, and he often humanizes the defendant in \
+the face of others’ rash, biased judgements.")
+         
+
+st.subheader("Juror No. 2: Lee")
+st.image(image="https://raw.githubusercontent.com/DH6688/avatar_images/main/Lee.png", width=175)
+st.write("Lee is a very strong, very forceful, extremely opinionated man within whom can be detected a streak of \
+sadism. He is a humorless man who is \
+intolerant of opinions other than his own and accustomed to forcing his wishes and views upon others. He is the owner \
+of a messenger service and a father who is estranged from his 20-year-old son. Lee is fully convinced of the \
+defendant’s guilt from the outset. He’s easily frustrated and quick to anger, and he resists the efforts of Davis \
+and others to take time with the process. He’s staunchly convinced that he is right, as feelings about his own son color his judgement.")
+         
+st.subheader("Juror No. 3: Tony")
+st.image(image="https://raw.githubusercontent.com/DH6688/avatar_images/main/Tony.png", width=175)
+st.write("Tony seems to be a man of wealth and position. He is a practiced speaker who presents himself well at all times.\
+ He seems to feel a little bit above the rest of the jurors. His only concern is with the facts in this case and he is appalled \
+ at the behavior of the others. He is a broker. Tony is logical and persuaded by reason. At the same time, he harbors classist \
+views of the defendant, and he believes the boy more likely to be guilty because he is poor.")
+         
+st.subheader("Juror No. 4: Jack")
+st.image(image="https://raw.githubusercontent.com/DH6688/avatar_images/main/Jack.png", width=175)
+st.write("Jack is a loud, flashy-handed salesman type who believes he has more \
+important things to do than to sit on a jury. He is quick to show temper, quick to form opinions on things about which he knows nothing. \
+He is a bully and a coward. Jack is an energetic jokester who is more interested in getting to his baseball game than sitting in \
+the jury room. He’s impatient for the proceedings to get over with and at first votes “guilty” with the majority.")
+         
+st.subheader("Juror No. 5: McCardle")
+st.image(image="https://raw.githubusercontent.com/DH6688/avatar_images/main/McCardle.png", width=175)
+st.write("McCardle is a mild gentle old man long since defeated by life and now merely waiting to die. \
+A man who recognizes himself for what he is and mourns the days when it would have been possible to be courageous without shielding himself behind \
+his many years. An old man and an early vote to acquit the boy. McCardle is thoughtful and compassionate, and he closely observes another old man \
+who testifies against the boy, seeing his loneliness and need to feel important. These feelings resonate with McCardle. He also is intolerant of \
+other jurors’ racism.")
+         
+st.subheader("Juror No. 6: Ed")
+st.image(image="https://raw.githubusercontent.com/DH6688/avatar_images/main/Ed.png", width=175)
+st.write("Ed is an angry, bitter man. He is man who antagonizes almost at sight. A bigot who places no values on any human life \
+save his own, a man who has been nowhere and is going nowhere and knows it deep within him. An owner of car repair shops and a vicious racist. Ed \
+doesn’t care about due process or the law and is on a single-minded mission to convict the boy because of his race. Angry, self-righteous, and \
+unyielding, he sees his job as protecting America from immigrants.")
+         
+st.subheader("Juror No. 7: Martin")
+st.image(image="https://raw.githubusercontent.com/DH6688/avatar_images/main/Martin.png", width=175)
+st.write("Martin is a small, petty man who is impressed with the authority he has and handles himself quite formally. \
+Martin is not overly bright, but dogged. Martin is a high school football coach. He takes his role seriously and attempts to maintain order throughout \
+the heated proceedings.")
+         
+st.subheader("Juror No. 8: Robert")
+st.image(image="https://raw.githubusercontent.com/DH6688/avatar_images/main/Robert.png", width=175)
+st.write("Robert is a slick, bright advertising man who thinks of human beings in terms of percentages, graphs, \
+and polls and has no real understanding of people. A superficial snob, but trying to be a good fellow. Robert is an ad man who likes to doodle. He is more \
+timid and relents easily when others push back against his ideas. He is the only juror to go back and forth on his vote and is overwhelmed by the \
+amount of evidence he has to grapple with.")
+         
+st.subheader("Juror No. 9: John")
+st.image(image="https://raw.githubusercontent.com/DH6688/avatar_images/main/John.png", width=175)
+st.write("John is a meek, hesitant man who finds it difficult to maintain any opinions of his own. \
+John is easily swayed and usually adopts the opinion of the last person to whom he has spoken. A father who works at a bank. Timid \
+and hesitant to speak, John hangs back for most of the proceedings. When he does talk, he is interrupted and talked \
+over by stronger personalities.")
+         
+st.subheader("Juror No. 10: Marshall")
+st.image(image="https://raw.githubusercontent.com/DH6688/avatar_images/main/Marshall.png", width=175)
+st.write("Marshall is a naive, very frightened \
+young man who takes his obligations in this case very seriously but, who finds it difficult to speak up when his elders have the floor. \
+He is a night nurse at a hospital in Harlem who grew up in a poor neighborhood. He is offended at the generalizations that some of the \
+other jurors make about the defendant because he grew up in a slum.")
+         
+st.subheader("Juror No. 11: Edward")
+st.image(image="https://raw.githubusercontent.com/DH6688/avatar_images/main/Edward.png", width=175)
+st.write("Edward is an honest but dull-witted man who comes upon his \
+decisions slowly and carefully. Edward is a man who finds it difficult to create positive opinions, but who must listen to and digest and accept \
+those opinions offered by others which appeal to him most. Edward is a house painter. Edward is convinced that the boy is guilty from the \
+start and remains convinced for much of the deliberation. He is focused primarily on the motive for the murder. He’s also very protective \
+of McCardle and stands up for him when others disrespect him.")
+
+st.subheader("Juror No. 12: George")
+st.image(image="https://raw.githubusercontent.com/DH6688/avatar_images/main/George.png", width=175)
+st.write("George is a refugee from Europe who has come to this country in 1941. A man \
+who speaks with an accent and who is ashamed humble, almost subservient to the people around him, but who will honestly seek justice because he has \
+suffered through so much injustice.")
